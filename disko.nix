c16b52e6581909f7f90791de9382ab58b385cffe
Eric-McKinney <ericmckinney.md@gmail.com>
2024-09-02 15:04:50 -0400

    Initial commit

---
+++ disko.nix
@@ -0,0 +1,41 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "";
+        type = "";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            swap = {
+              size = "4G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
