797b5e70f817466d597178bafb35d8a90f698bc3
Eric-McKinney <ericmckinney.md@gmail.com>
2024-09-02 21:56:16 -0400

    Fix console font

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "jetbrains-mono";
+    font = "Lat2-Terminus16";
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   };
